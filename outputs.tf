6fdf0a0c0d4eb69e7a0dc7beb1c009a4b6558f40
Rajesh Aradhye <raradhye@gmail.com>
2026-03-26 12:35:39 -0700

    add outputs for VNET Id's and private IP's of VM

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+output "hub_vnet_id" {
+  description = "Hub VNet ID"
+  value       = azurerm_virtual_network.hub.id
+}
+output "spoke_vnet_id" {
+  description = "Spoke VNet Id"
+  value       = azurerm_virtual_network.spoke_dev.id
+}
+output "hub_vm_private_ip" {
+  description = "Hub VM Private IP"
+  value       = azurerm_linux_virtual_machine.hub_vm.private_ip_address
+}
+output "spoke_vm_private_ip" {
+  description = "Spoke VM Private IP"
+  value       = azurerm_linux_virtual_machine.spoke_vm.private_ip_address
+}
